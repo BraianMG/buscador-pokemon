ce01b8a56b9c20bf4e0196098a2f13e672bf687a
BraianMG <braian.gonzales77@gmail.com>
2021-12-10 19:17:02 -0300

    slight general adjustments

--- react/src/components/Footer.js
+++ react/src/components/Footer.js
@@ -3,14 +3,16 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="container mx-auto my-5 flex flex-wrap justify-around">
-        <div data-cy="proyect-author" className="text-center text-2xl text-white font-bold copy my-5">
+        <a data-cy="proyect-author" className="text-center text-2xl text-white font-bold copy my-5"
+            href="https://braiangonzales.netlify.app/" target="_blank" rel="noopener noreferrer"
+        >
             Hecho por Braian Gonzales
-        </div>
+        </a>
         <div className="my-5">
             <a 
                 data-cy="proyect-repo"
                 className="block w-full bg-blue-800 hover:bg-blue-500 text-xl text-white font-bold text-center rounded py-2 px-10"
-                href="https://github.com/BraianMG/" target="_blank" rel="noopener noreferrer">
+                href="https://github.com/BraianMG/buscador-pokemon" target="_blank" rel="noopener noreferrer">
                 Link a mi repo
             </a>
         </div>
